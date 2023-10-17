94ad09cb5be485c038c8f4aadc8089b12c3e420d
Devansh Parapalli <parapallidev@gmail.com>
2023-10-17 22:08:31 +0530

    Added Image Optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), ViteImageOptimizer({
+    png: {
+      quality: 60,
+    },
+    webp: {
+      quality: 60,
+    },
+    jpeg: {
+      quality: 60,
+    },
+    jpg: {
+      quality: 60,
+    }
+  })],
 };
 
 export default config;
